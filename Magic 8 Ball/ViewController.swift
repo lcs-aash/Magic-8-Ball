23de38ee0dcfd8c989a4d361939ca87395f3e011
Rori Ash <aash@lcs.on.ca>
2019-10-26 11:13:02 -0400

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Ash, Aurora on 2019-10-26.
+//  Copyright © 2019 Rori Ash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
